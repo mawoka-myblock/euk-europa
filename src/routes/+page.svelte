161be2bf2429f59f36ad59897813a9c305254b1a
Mawoka <git@mawoka.eu>
2025-02-02 22:18:19 +0100

    :bug: Spacing on landing page on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 
 <section class="flex min-h-screen w-screen">
 	<div class="m-auto flex flex-col items-center">
-		<h1 class="marck-script text-8xl">Marlon's Project for the European Competition</h1>
+		<h1 class="marck-script text-8xl text-center">Marlon's Project for the European Competition</h1>
 		<p>How <i>You</i> can help to save food</p>
 	</div>
 </section>
@@ -14,7 +14,7 @@
 	<h2 class="marck-script mx-auto text-6xl">The problem</h2>
 	<p class="mx-auto">We're all familiar with the problem, right? Right?</p>
 
-	<p class="mx-auto mt-4 lg:w-1/2">
+	<p class="lg:mx-auto mt-4 lg:w-1/2 m-2">
 		If not, here's a small recap: The way most of us eat is not possible if everyone were to eat
 		like you are. And if you know think of something like "you're better, so you deserve that", then
 		get the fck out of here! So, the food we eat is too inefficient to produce in that scale so that
@@ -28,13 +28,13 @@
 	<h2 class="marck-script mx-auto text-6xl">This website...</h2>
 	<p class="mx-auto">What this here is all about</p>
 
-	<p class="mx-auto mt-4 lg:w-1/2">
+	<p class="lg:mx-auto mt-4 lg:w-1/2 m-2">
 		Once you've clicked yourself through this website, you should have learned quite a bit about
 		land usage of food. But not just about you and your local eating habits, but also a bit more
 		about eating diets in general. Enough theory, let's get right in!
 	</p>
 
-	<div class="mx-auto m-2 text-2xl">
+	<div class="mx-auto m-6 text-2xl">
 		<BrownButton href="/journey/start">Get Started</BrownButton>
 	</div>
 </section>
