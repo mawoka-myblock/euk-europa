da5c94f415f3faa0692b4501f93c78ba1c4d07f6
Mawoka <git@mawoka.eu>
2025-03-17 12:55:01 +0100

    Package manager fix

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
 <footer class="flex flex-col bg-gray-600 text-white">
 	<p class="mx-auto">Sources can be found on the <a href="/sources" class="underline">source page.</a></p>
-	<p class="mx-auto">Made with ❤️ by <a href="https://mawoka.eu" target="_blank" class="underline">Mawoka</a></p>
+	<!-- <p class="mx-auto">Made with ❤️ by <a href="https://mawoka.eu" target="_blank" class="underline">Mawoka</a></p> -->
 </footer>
